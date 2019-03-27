253d89bce448cbfc50558b98b3e4f27eb38da480
Nithin Gajjala <nithin.gajjala@FIADMIN.COM>
2019-03-26 18:24:23 -0700

    updatedmessage

--- Demo.java
+++ Demo.java
@@ -1,16 +1,6 @@
 public class Demo {
 
     public static void main(String[] args) {
-
-       int num = 10, count = 1, total = 0;
-
-       while(count <= num)
-       {
-           total = total + count;
-           count++;
-       }
-
-       System.out.println("Sum of first 10 natural numbers is: "+total);
        System.out.println("Welcome Back Nithin Reddy Gajjala");
     }
 }
